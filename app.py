eb272fc096cd87d27f030dd865c14b36b363dd9b
Dashuko <94691148+Real-Dashuk0-28@users.noreply.github.com>
2026-05-04 11:00:39 +0900

    Страница "Правила"

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-app.run(debug=True)
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
